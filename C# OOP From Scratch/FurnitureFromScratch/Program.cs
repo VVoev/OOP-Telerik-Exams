fb7a444413860decddf32d461cca53697469b3b3
VVoev <vladovoev@yahoo.co.uk>
2017-01-14 18:02:20 +0200

    singleton added

--- C# OOP From Scratch/FurnitureFromScratch/Program.cs
+++ C# OOP From Scratch/FurnitureFromScratch/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FurnitureFromScratch.Engine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,8 @@ namespace FurnitureFromScratch
     {
         static void Main()
         {
+            //Singleton Implementation
+            FurnitureManufacturerEngine.Instance.Start();
         }
     }
 }
